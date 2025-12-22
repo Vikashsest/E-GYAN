aa93e3ea3316f5c02e4f81921ff5fce7cd104c76
Vikashsest <vikas@sestinfotech.com>
2025-12-22 11:32:08 +0530

    add

--- backend/src/modules/class/class.controller.ts
+++ backend/src/modules/class/class.controller.ts
@@ -1,8 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ClassService } from './class.service';
 import { CreateClassDto } from './dto/create-class.dto';
 import { UpdateClassDto } from './dto/update-class.dto';
-
 @Controller('class')
 export class ClassController {
   constructor(private readonly classService: ClassService) {}
