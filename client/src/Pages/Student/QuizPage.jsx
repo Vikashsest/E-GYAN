bee4da6ca303089e79b5378529f349f373062906
Vikashsest <vikas@sestinfotech.com>
2026-07-23 08:14:41 +0530

    changes as for requiremnt

--- client/src/Pages/Student/QuizPage.jsx
+++ client/src/Pages/Student/QuizPage.jsx
@@ -3,15 +3,14 @@ import { FaClock, FaQuestionCircle, FaStar } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 export default function QuizPage() {
   const { id } = useParams();
- const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <div className="min-h-screen bg-[#1e1f2b] text-white p-8 flex flex-col items-center">
-      {/* Card */}
       <div className="bg-gradient-to-br from-[#2e2f44] to-[#1f202f] rounded-2xl shadow-lg p-8 w-full max-w-3xl border border-white/10">
         <h1 className="text-3xl font-bold mb-4">🎯 Quiz {id}</h1>
         <p className="text-gray-300 mb-6">
-          Get ready for a quick <span className="text-yellow-400">Science</span> quiz!  
-          Test your knowledge in a fun way 🎉
+          Get ready for a quick <span className="text-yellow-400">Science</span>{" "}
+          quiz! Test your knowledge in a fun way 🎉
         </p>
 
         {/* Info Section */}
@@ -45,10 +44,10 @@ export default function QuizPage() {
 
         {/* Start Button */}
         {/* <button className="w-full py-3 bg-blue-600 hover:bg-blue-700 rounded-lg font-bold text-lg transition-all duration-300"> */}
-         <button
-        onClick={() => navigate("/student/quiz/start")}
-        className="px-4 py-2 bg-green-600 rounded-lg hover:bg-green-700 text-white"
-      >
+        <button
+          onClick={() => navigate("/student/quiz/start")}
+          className="px-4 py-2 bg-green-600 rounded-lg hover:bg-green-700 text-white"
+        >
           🚀 Start Quiz
         </button>
       </div>
